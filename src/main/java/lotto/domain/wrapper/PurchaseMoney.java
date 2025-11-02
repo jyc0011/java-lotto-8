c98922e211c67042547f4be9bf0cfd06b0342dea
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-11-02 20:10:02 +0900

    feat:  구매금액 wrapper 추가
    구매 금액의 wrapper의 테스트와 클래스 추가

--- src/main/java/lotto/domain/wrapper/PurchaseMoney.java
+++ src/main/java/lotto/domain/wrapper/PurchaseMoney.java
@@ -1,4 +1,63 @@
 package lotto.domain.wrapper;
 
+import lotto.view.ErrorMessage;
+
+/**
+ * 로또 구입 금액 wrapper
+ * 생성할 때 1,000원 단위 및 1,000원 이상 여부 검증
+ */
 public class PurchaseMoney {
+    private final int amount;
+
+    private static final int LOTTO_PRICE = 1000;
+
+    /**
+     * 구매 금액 생성
+     *
+     * @param amount 구매 금액
+     * @throws IllegalArgumentException 유효하지 않은 형식 또는 금액 규칙 위반 시
+     */
+    public PurchaseMoney(int amount) {
+        validateUnit(amount);
+        validateMinimum(amount);
+        this.amount = amount;
+    }
+
+    /**
+     *1000원 단위인지 확인
+     */
+    private void validateUnit(int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            // TODO: ErrorMessage Enum에서 메시지 가져오기
+            throw new IllegalArgumentException("[ERROR] 금액은 1,000원 단위여야 합니다.");
+        }
+    }
+
+    /**
+     * 최소 금액 이상인지 확인
+     */
+    private void validateMinimum(int amount) {
+        if (amount < LOTTO_PRICE) {
+            // TODO: ErrorMessage Enum에서 메시지 가져오기
+            throw new IllegalArgumentException("[ERROR] 최소 구입 금액은 1,000원입니다.");
+        }
+    }
+
+    /**
+     * 구매 로또 개수 리턴
+     *
+     * @return 로또 개수
+     */
+    public int getLottoCount() {
+        return amount / LOTTO_PRICE;
+    }
+
+    /**
+     * 구매 금액 리턴
+     *
+     * @return 구매 금액
+     */
+    public int getAmount() {
+        return amount;
+    }
 }
--- src/test/java/lotto/domain/wrapper/PurchaseMoneyTest.java
+++ src/test/java/lotto/domain/wrapper/PurchaseMoneyTest.java
@@ -1,7 +1,33 @@
 package lotto.domain.wrapper;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@DisplayName("구매 금액 테스트")
 class PurchaseMoneyTest {
 
+    @ParameterizedTest
+    @CsvSource({"1000, 1", "8000, 8", "120000, 120"})
+    @DisplayName("성공 : 유효한 금액 입력")
+    void createPurchaseMoney_Success(int amount, int expectedCount) {
+        PurchaseMoney money = new PurchaseMoney(amount);
+        assertThat(money.getLottoCount()).isEqualTo(expectedCount);
+        assertThat(money.getAmount()).isEqualTo(amount);
+    }
+
+    @ParameterizedTest(name = "{displayName} - 입력값: {0}")
+    @ValueSource(ints = {1001, 999, 500, -1000, 0})
+    @DisplayName("실패 : 유효하지 않은 입력(규칙, 형식, null) 시 생성 불가")
+    void createPurchaseMoney_Fail_AllInvalidInputs(int invalidAmount) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new PurchaseMoney(invalidAmount))
+                .withMessageContaining("[ERROR]");
+    }
 }
