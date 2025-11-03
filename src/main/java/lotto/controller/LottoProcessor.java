163190326071b22f13b3a7dfbb0354fcde46e0a0
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-11-03 18:04:45 +0900

    feat:  LottoProcessor 구현
    Facade Pattern을 통해 컨트롤러 단에서 너무 많은 인스턴스 변수를 가지지 않게 조절

--- src/main/java/lotto/controller/LottoProcessor.java
+++ src/main/java/lotto/controller/LottoProcessor.java
@@ -1,4 +1,89 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.dto.PlayerPurchaseDto;
+import lotto.domain.wrapper.WinningCombo;
+import lotto.dto.StatDto;
+import lotto.domain.wrapper.BonusNumber;
+import lotto.domain.wrapper.PurchaseMoney;
+import lotto.service.LottoService;
+import lotto.util.InputParser;
+
+/**
+ * 서비스 호출과 파싱등을 호출
+ */
 public class LottoProcessor {
+
+    private final LottoService lottoService;
+    private final InputParser parser;
+
+    /**
+     * 로직 Facade를 생성합니다.
+     *
+     * @param lottoService
+     * @param parser
+     */
+    public LottoProcessor(LottoService lottoService, InputParser parser) {
+        this.lottoService = lottoService;
+        this.parser = parser;
+    }
+
+    /**
+     * 문자열 입력 파싱, 검증, PurchaseMoney 객체 생성
+     *
+     * @param input 입력 문자열
+     * @return 유효성이 검증된 PurchaseMoney 객체
+     * @throws IllegalArgumentException
+     */
+    public PurchaseMoney createPurchaseMoney(String input) {
+        int amount = parser.parseInt(input);
+        return new PurchaseMoney(amount);
+    }
+
+    /**
+     * 문자열 입력 파싱, 검증, 당첨 Lotto 객체 생성
+     *
+     * @param input 입력 문자열
+     * @return 유효성이 검증된 Lotto 객체
+     * @throws IllegalArgumentException
+     */
+    public Lotto createWinningLotto(String input) {
+        List<Integer> numbers = parser.parseWinningNumbers(input);
+        return new Lotto(numbers);
+    }
+
+    /**
+     * 문자열 입력 파싱, 검증, BonusNumber 객체 생성
+     *
+     * @param input        입력 문자열
+     * @param winningLotto 중복 검증을 위한 당첨 번호
+     * @return 유효성이 검증된 BonusNumber 객체
+     * @throws IllegalArgumentException
+     */
+    public BonusNumber createBonusNumber(String input, Lotto winningLotto) {
+        int number = parser.parseInt(input);
+        return new BonusNumber(number, winningLotto);
+    }
+
+    /**
+     * LottoService에 로또 생성 위임
+     *
+     * @param money 구매 금액
+     * @return 생성된 로또 묶음
+     */
+    public PlayerPurchaseDto purchaseLottos(PurchaseMoney money) {
+        Lottos lottos = lottoService.purchaseLottos(money);
+        return new PlayerPurchaseDto(lottos, money);
+    }
+
+    /**
+     * LottoService에 통계 계산 위임
+     *
+     * @return 통계 DTO
+     */
+    public StatDto calculateStatistics(PlayerPurchaseDto purchase, WinningCombo winningCombo) {
+        return lottoService.calculateStatistics(purchase.getLottos(), winningCombo, purchase.getMoney());
+    }
 }
---
+++ src/test/java/lotto/controller/LottoProcessorTest.java
@@ -0,0 +1,77 @@
+package lotto.controller;
+
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.dto.PlayerPurchaseDto;
+import lotto.domain.wrapper.WinningCombo;
+import lotto.dto.StatDto;
+import lotto.domain.factory.LottoFactory;
+import lotto.domain.strategy.AutoGenStrategy;
+import lotto.domain.strategy.LottoGenStrategy;
+import lotto.domain.wrapper.BonusNumber;
+import lotto.domain.wrapper.PurchaseMoney;
+import lotto.service.LottoService;
+import lotto.util.InputParser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@DisplayName("LottoProcessor 테스트")
+class LottoProcessorTest {
+
+    private LottoProcessor processor;
+
+    @BeforeEach
+    void setUp() {
+        InputParser parser = new InputParser();
+        LottoGenStrategy strategy = new AutoGenStrategy();
+        LottoFactory factory = new LottoFactory(strategy);
+        LottoService service = new LottoService(factory);
+        processor = new LottoProcessor(service, parser);
+    }
+
+    @Test
+    @DisplayName("실패: parser에서 PurchaseMoney 생성 실패")
+    void createPurchaseMoney_Fail_InvalidFormat() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> processor.createPurchaseMoney("abc"))
+                .withMessageContaining("[ERROR]");
+    }
+
+    @Test
+    @DisplayName("실패: wrapper에서 PurchaseMoney 생성 실패")
+    void createPurchaseMoney_Fail_InvalidRule() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> processor.createPurchaseMoney("999"))
+                .withMessageContaining("[ERROR]");
+    }
+
+    @Test
+    @DisplayName("성공 : 로또 생성")
+    void purchaseLottos_IntegrationSuccess() {
+        PurchaseMoney money = new PurchaseMoney(3000);
+        PlayerPurchaseDto purchase = processor.purchaseLottos(money);
+        assertThat(purchase.getLottos().size()).isEqualTo(3);
+        assertThat(purchase.getMoney()).isEqualTo(money);
+    }
+
+    @Test
+    @DisplayName("성공 : Service와 Stat으로 통계 계산")
+    void calculateStatistics_IntegrationSuccess() {
+        PurchaseMoney money = new PurchaseMoney(1000);
+        Lotto fifthPrizeLotto = new Lotto(List.of(1, 2, 3, 10, 11, 12));
+        Lottos lottos = new Lottos(List.of(fifthPrizeLotto));
+        PlayerPurchaseDto purchase = new PlayerPurchaseDto(lottos, money);
+
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(7, winningLotto);
+        WinningCombo winningCombo = new WinningCombo(winningLotto, bonusNumber);
+        StatDto result = processor.calculateStatistics(purchase, winningCombo);
+        assertThat(result.getFormattedProfitRate()).isEqualTo("500.0%");
+    }
+}
