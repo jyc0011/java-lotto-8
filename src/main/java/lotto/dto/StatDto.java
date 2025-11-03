35c4badf8aff41ba73195f2d313343424447f105
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-11-03 17:57:18 +0900

    feat:  stat dto 추가
    통계 dto와 테스트 추가

--- src/main/java/lotto/dto/StatDto.java
+++ src/main/java/lotto/dto/StatDto.java
@@ -1,4 +1,45 @@
 package lotto.dto;
 
+import lotto.domain.Rank;
+import lotto.domain.wrapper.Profit;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Stat 계산 결과를 View 계층으로 전달하는 DTO
+ */
 public class StatDto {
+
+    private final Map<Rank, Long> rankCounts;
+    private final Profit profit;
+
+    /**
+     * 통계 결과 DTO 생성
+     *
+     * @param rankCounts 등수별 당첨 횟수 맵
+     * @param profit 계산된 수익률 객체
+     */
+    public StatDto(Map<Rank, Long> rankCounts, Profit profit) {
+        this.rankCounts = Collections.unmodifiableMap(rankCounts);
+        this.profit = profit;
+    }
+
+    /**
+     * 등수별 당첨 횟수 출력
+     *
+     * @return Map (Key: Rank, Value: 횟수)
+     */
+    public Map<Rank, Long> getRankCounts() {
+        return rankCounts;
+    }
+
+    /**
+     * 수익률 출력
+     *
+     * @return ProfitRate 래퍼 객체
+     */
+    public String getFormattedProfitRate() {
+        return profit.formattedRate();
+    }
 }
---
+++ src/test/java/lotto/dto/StatDtoTest.java
@@ -0,0 +1,27 @@
+package lotto.dto;
+
+import lotto.domain.Rank;
+import lotto.domain.wrapper.Profit;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("StatDto 테스트")
+class StatDtoTest {
+
+    @Test
+    @DisplayName("성공 : 생성자로 전달된 데이터가 getter로 확인됨")
+    void createAndGet_Success() {
+        Map<Rank, Long> rankCounts = new EnumMap<>(Rank.class);
+        rankCounts.put(Rank.FIFTH, 1L);
+        Profit profit = new Profit(5000L, 8000);
+        String expectedFormattedRate = "62.5%";
+        StatDto dto = new StatDto(rankCounts, profit);
+        assertThat(dto.getRankCounts()).isEqualTo(rankCounts);
+        assertThat(dto.getFormattedProfitRate()).isEqualTo(expectedFormattedRate);
+    }
+}
