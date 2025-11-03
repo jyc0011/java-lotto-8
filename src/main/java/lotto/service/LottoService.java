3a253b3bce2ff4fc90a2c6ba54b49f905d3f9548
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-11-03 18:04:20 +0900

    feat:  LottoService 구현
    lotto 관련 기능과 로직에 대해 수행하는 객체이ㅔ게 위임

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,48 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.domain.Stat;
+import lotto.domain.wrapper.WinningCombo;
+import lotto.dto.StatDto;
+import lotto.domain.factory.LottoFactory;
+import lotto.domain.wrapper.BonusNumber;
+import lotto.domain.wrapper.PurchaseMoney;
+
+/**
+ * 서비스
+ */
 public class LottoService {
+    private final LottoFactory lottoFactory;
+
+    /**
+     * LottoFactory 주입, 객체 생성
+     * @param lottoFactory
+     */
+    public LottoService(LottoFactory lottoFactory) {
+        this.lottoFactory = lottoFactory;
+    }
+
+    /**
+     * 구매 금액으로 로또 생성, 해당 부분을 factory에 위임
+     *
+     * @param money 구매 금액 (Wrapper)
+     * @return 생성된 로또 묶음 (일급 컬렉션)
+     */
+    public Lottos purchaseLottos(PurchaseMoney money) {
+        return lottoFactory.createLottos(money);
+    }
+
+    /**
+     * 당첨 통계 계산을 stat에 위임
+     *
+     * @param purchasedLottos 구매한 로또 묶음
+     * @param winningCombo    당첨 조합 (Lotto + BonusNumber)
+     * @param money           구매 금액
+     * @return 통계 결과 DTO
+     */
+    public StatDto calculateStatistics(Lottos purchasedLottos, WinningCombo winningCombo, PurchaseMoney money) {
+        Stat stat = new Stat();
+        return stat.calculate(purchasedLottos, winningCombo, money);
+    }
 }
---
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -0,0 +1,48 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.domain.Rank;
+import lotto.domain.wrapper.WinningCombo;
+import lotto.dto.StatDto;
+import lotto.domain.factory.LottoFactory;
+import lotto.domain.strategy.AutoGenStrategy;
+import lotto.domain.strategy.LottoGenStrategy;
+import lotto.domain.wrapper.BonusNumber;
+import lotto.domain.wrapper.PurchaseMoney;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("로또 서비스(LottoService) 테스트")
+class LottoServiceTest {
+
+    @Test
+    @DisplayName("성공 : Stat 계산하여 DTO 출력")
+    void calculateStatistics_ShouldOrchestrate_StatCalculation() {
+        LottoService lottoService = new LottoService(null);
+        PurchaseMoney money = new PurchaseMoney(1000);
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(7, winningLotto);
+        WinningCombo winningCombo = new WinningCombo(winningLotto,bonusNumber);
+        Lotto fifthPrizeLotto = new Lotto(List.of(1, 2, 3, 10, 11, 12));
+        Lottos lottos = new Lottos(List.of(fifthPrizeLotto));
+        StatDto result = lottoService.calculateStatistics(lottos, winningCombo, money);
+        assertThat(result.getRankCounts().get(Rank.FIFTH)).isEqualTo(1L);
+        assertThat(result.getFormattedProfitRate()).isEqualTo("500.0%");
+    }
+
+    @Test
+    @DisplayName("성공 : 팩토리를 생성해 구매 금액만큼 로또 생성")
+    void purchaseLottos_ShouldCallFactory_AndReturnLottos() {
+        LottoGenStrategy strategy = new AutoGenStrategy();
+        LottoFactory factory = new LottoFactory(strategy);
+        LottoService lottoService = new LottoService(factory);
+        PurchaseMoney money = new PurchaseMoney(8000);
+        Lottos lottos = lottoService.purchaseLottos(money);
+        assertThat(lottos.size()).isEqualTo(8);
+    }
+}
